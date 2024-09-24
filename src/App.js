cb299bcc9afec30ca12c40da02c1b1bcd4e5f111
Andrew Dinh <andrewdinh74@yahoo.com>
2024-09-24 16:14:38 -0700

    Update App component

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
     <div className="categories-container">
       {categories.map(({ title, id }) => (
         <div className="category-container" key={id}>
-          {/* <img/> */}
+          <div className="background-image"></div>
           <div className="category-body-container">
             <h2>{title}</h2>
             <p>Shop Now</p>
