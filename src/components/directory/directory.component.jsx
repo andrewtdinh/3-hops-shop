ca995d4aaa30380073e70284b30ab906ea699fd3
Andrew Dinh <andrewdinh74@yahoo.com>
2024-09-25 16:42:36 -0700

    Fix the styles and key issue

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,10 +1,11 @@
 import CategoryItem from "../category-item/category-item.component";
+import './directory.styles.scss'
 
 const Directory = ({ categories }) => {
   return (
-    <div className="categories-container">
+    <div className="directory-container">
       {categories.map((category) => (
-        <CategoryItem category={category} />
+        <CategoryItem category={category} key={category.id}/>
       ))}
     </div>
   );
