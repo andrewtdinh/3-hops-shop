63f4a78a6d6befc9a878421d4ddec4aa3231b550
Andrew Dinh <andrewdinh74@yahoo.com>
2024-09-28 16:08:32 -0700

    Make Navigation into a route component and make Home the index route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
-import { Routes, Route, Outlet } from "react-router-dom";
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the nav bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  )
-}
+import Navigation from "./routes/navigation/navigation.component";
 
 const Shop = () => {
   return (
@@ -24,7 +14,7 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />} >
-        <Route path='home' element={<Home />} />
+        <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
       </Route>
     </Routes>
