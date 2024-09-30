d8072680ab2034831d34f472b73a57012692835f
Andrew Dinh <andrewdinh74@yahoo.com>
2024-09-30 16:20:34 -0700

    Create a firebaseApp instance

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBpDOAvLrlDMMCRRTscr7qqbRUKN4EkHII",
+  authDomain: "three-hops-shop.firebaseapp.com",
+  projectId: "three-hops-shop",
+  storageBucket: "three-hops-shop.appspot.com",
+  messagingSenderId: "876482900560",
+  appId: "1:876482900560:web:0cd52aafacc47396e95755"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
