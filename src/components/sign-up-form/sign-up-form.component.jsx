247f535abbefb228081dae52ca13575e0c8ea88a
Andrew Dinh <andrewdinh74@yahoo.com>
2024-10-24 09:35:17 -0700

    Clear user signup form after successful submission

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -13,6 +13,10 @@ const SignUpForm = () => {
   const [ formFields, setFormFields ] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields)
+  }
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -26,6 +30,7 @@ const SignUpForm = () => {
       const { user } = await createAuthUserWithEmailAndPassword(email, password);
 
       await createUserDocumentFromAuth(user, { displayName })
+      resetFormFields();
     } catch (error) {
       if (error.code === 'auth/email-already-in-use') {
         alert('Cannot create user.  Email already in use')
