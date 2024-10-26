58d7233a53c8a9ff8e1ac55a6ba21a304559ba6a
Andrew Dinh <andrewdinh74@yahoo.com>
2024-10-26 14:32:47 -0700

    Remove SignInWithGoogleRedirect button

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { getRedirectResult } from 'firebase/auth';
-import { auth, signInWithGooglePopUp, signInWithGoogleRedirect, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { auth, signInWithGooglePopUp, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
@@ -22,16 +22,15 @@ const Authentication = () => {
     const userDocRef = await createUserDocumentFromAuth(user)
   }
 
-  const logGoogleRedirectUser = async () => {
-    const { user } = await signInWithGoogleRedirect();
-    const userDocRef = await createUserDocumentFromAuth(user)
-  }
+  // const logGoogleRedirectUser = async () => {
+  //   const { user } = await signInWithGoogleRedirect();
+  //   const userDocRef = await createUserDocumentFromAuth(user)
+  // }
 
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      <button onClick={logGoogleRedirectUser}>Sign in with Google Redirect</button>
       <SignUpForm/>
     </div>
   )
