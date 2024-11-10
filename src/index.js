2bf3c84522c3a4a63e84fe8c29eba6d98d0d25f9
Andrew Dinh <andrewdinh74@yahoo.com>
2024-11-09 17:28:30 -0800

    Replace mock data with ProductsContext

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
-import App from './App';
-import { UserProvider } from './contexts/user.context';
+import App from "./App";
+import { UserProvider } from "./contexts/user.context";
+import { ProductsProvider } from "./contexts/products.context";
 
-import './index.scss';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.scss";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-         <App /> 
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
