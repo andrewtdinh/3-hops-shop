6522f90c6c8cbded368be4a45db132172e702c48
Andrew Dinh <andrewdinh74@yahoo.com>
2024-11-13 17:05:33 -0800

    Create Cart Icon component

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -4,14 +4,17 @@ import Button from "../button/button.component";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
-  <div className="product-card-container">
-    <img src={imageUrl} alt={`${name}`} />
-    <div className="footer">
-      <div className="name">{name}</div>
-      <div className="price">{price}</div>
+  
+  return (
+    <div className="product-card-container">
+      <img src={imageUrl} alt={`${name}`} />
+      <div className="footer">
+        <div className="name">{name}</div>
+        <div className="price">{price}</div>
+      </div>
+      <Button buttonType="inverted">Add to Cart</Button>
     </div>
-    <Button buttonType="inverted">Add to Cart</Button>
-  </div>;
+  );
 };
 
 export default ProductCard;
