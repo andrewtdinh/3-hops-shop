e43b77500d57a33aab278ee44b0f1054216113e8
Andrew Dinh <andrewdinh74@yahoo.com>
2024-11-16 20:44:33 -0800

    Can add item to cart

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,10 +1,17 @@
 import "./product-card.styles.scss";
 
+import { useContext } from "react";
+
+import { CartContext } from "../../contexts/cart.context";
+
 import Button from "../button/button.component";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
-  
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
+
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
@@ -12,7 +19,7 @@ const ProductCard = ({ product }) => {
         <div className="name">{name}</div>
         <div className="price">{price}</div>
       </div>
-      <Button buttonType="inverted">Add to Cart</Button>
+      <Button buttonType="inverted" onClick={addProductToCart} >Add to Cart</Button>
     </div>
   );
 };
