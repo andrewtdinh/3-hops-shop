3f73d1f23013ef93eecb62ec56f46a61bc6226a6
Andrew Dinh <andrewdinh74@yahoo.com>
2025-01-26 16:07:49 -0800

    Items and quantities show up in cart page without styles

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import "./cart-dropdown.styles.scss";
+import { useNavigate } from "react-router-dom";
 
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
@@ -7,15 +8,19 @@ import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
 
 const CartDropdown = () => {
-
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate('/checkout')
+  }
 
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
         {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)}
       </div>
-      <Button>CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
     </div>
   );
 };
---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,29 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
+
+import './checkout.styles.scss'
+
+const Checkout = () => {
+  const { cartItems } = useContext(CartContext);
+
+
+  return (
+    <div>
+      <h1>I am the Checkout Page</h1>
+      <div>
+        {
+          cartItems.map((cartItem) => {
+            const { id, name, quantity} = cartItem;
+            return <div key={id}>
+              <h2>{name}</h2>
+              <span>{quantity}</span>
+            </div>
+          })
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Checkout;
