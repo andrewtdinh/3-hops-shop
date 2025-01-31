9ce3261a2e30ef43a205cf241c4362fe6963521c
Andrew Dinh <andrewdinh74@yahoo.com>
2025-01-31 15:05:05 -0800

    Bring cart total into the checkout page

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -7,7 +7,7 @@ import "./checkout.styles.scss";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 
 const Checkout = () => {
-  const { cartItems } =
+  const { cartItems, cartTotal } =
     useContext(CartContext);
 
   return (
@@ -32,7 +32,7 @@ const Checkout = () => {
       {cartItems.map((cartItem) => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
       ))}
-      <span className="total">Total: 0</span>
+      <span className="total">Total: ${cartTotal}</span>
     </div>
   );
 };
